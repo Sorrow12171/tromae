de3b4c1ed86e88bf2f726a53bd3c3da973869367
Sorrow12171 <miku12171f@gmail.com>
2026-04-25 15:44:20 -0500

    Crear seccion 25_4

--- galeria/galeria_data_25.js
+++ galeria/galeria_data_25.js
@@ -150,6 +150,14 @@ const galeriaData_25 = {
             { id: 49, url: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD31/main/imagenes/img_1777143147760.png" }
         ]
     },
+
+    '25_4': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
 };
 
 if (typeof galeriaDatabase !== 'undefined') {
