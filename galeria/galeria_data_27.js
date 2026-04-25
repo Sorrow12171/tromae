1a729c41b411362cbc5d6b19f5bace93c1822a33
Sorrow12171 <miku12171f@gmail.com>
2026-04-25 16:00:52 -0500

    Crear galeria_data_27.js

---
+++ galeria/galeria_data_27.js
@@ -0,0 +1,15 @@
+// GALERÍA - DATOS CHICA 27
+
+const galeriaData_27 = {
+    '27_1': {
+        titulo: "Classroom of the putas",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD31/main/imagenes/img_1777150848855.jpg",
+        imagenes: []
+    }
+};
+
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_27);
+}
