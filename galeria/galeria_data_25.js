68f3a5d0f823e25f54a24e04e6467f80542730e2
Sorrow12171 <miku12171f@gmail.com>
2026-04-25 12:48:47 -0500

    Crear galeria_data_25.js

---
+++ galeria/galeria_data_25.js
@@ -0,0 +1,15 @@
+// GALERÍA - DATOS CHICA 25
+
+const galeriaData_25 = {
+    '25_1': {
+        titulo: "Marin",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD30/main/imagenes/img_1777139325043.jpg",
+        imagenes: []
+    }
+};
+
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_25);
+}
