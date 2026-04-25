d5ece88c6ea9138b8468e1402cee6ee487ad8aa2
Sorrow12171 <miku12171f@gmail.com>
2026-04-25 15:52:04 -0500

    Crear galeria_data_26.js

---
+++ galeria/galeria_data_26.js
@@ -0,0 +1,15 @@
+// GALERÍA - DATOS CHICA 26
+
+const galeriaData_26 = {
+    '26_1': {
+        titulo: "Yuigahama",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD31/main/imagenes/img_1777150321679.webp",
+        imagenes: []
+    }
+};
+
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_26);
+}
