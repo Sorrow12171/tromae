b58ef17e5e5815fb1c0c5622ed392fa435e65522
Sorrow12171 <miku12171f@gmail.com>
2026-04-25 16:48:37 -0500

    Crear galeria_data_28.js

---
+++ galeria/galeria_data_28.js
@@ -0,0 +1,15 @@
+// GALERÍA - DATOS CHICA 28
+
+const galeriaData_28 = {
+    '28_1': {
+        titulo: "PutasDelZZZ",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD31/main/imagenes/img_1777153715050.webp",
+        imagenes: []
+    }
+};
+
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_28);
+}
