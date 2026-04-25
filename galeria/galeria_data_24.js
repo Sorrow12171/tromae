6d57915804a71ccc31a8e227b79e6c848be854b4
Sorrow12171 <miku12171f@gmail.com>
2026-04-25 11:50:06 -0500

    Crear galeria_data_24.js

---
+++ galeria/galeria_data_24.js
@@ -0,0 +1,15 @@
+// GALERÍA - DATOS CHICA 24
+
+const galeriaData_24 = {
+    '24_1': {
+        titulo: "Mitsuri",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD30/main/imagenes/img_1777135803747.jpg",
+        imagenes: []
+    }
+};
+
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_24);
+}
