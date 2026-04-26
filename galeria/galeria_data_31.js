ee67d57fce1fa571b8686d418963b5fb0ac384e9
Sorrow12171 <miku12171f@gmail.com>
2026-04-26 09:22:41 -0500

    Crear galeria_data_31.js

---
+++ galeria/galeria_data_31.js
@@ -0,0 +1,15 @@
+// GALERÍA - DATOS CHICA 31
+
+const galeriaData_31 = {
+    '31_1': {
+        titulo: "HonkaiStarRail",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD32/main/imagenes/img_1777213357793.png",
+        imagenes: []
+    }
+};
+
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_31);
+}
