f8d8729e791b65e906a1b0bc2c25842b7d103388
Sorrow12171 <miku12171f@gmail.com>
2026-04-26 07:50:46 -0500

    Crear seccion 6_27

--- galeria/galeria_data_6.js
+++ galeria/galeria_data_6.js
@@ -1842,6 +1842,14 @@ const galeriaData_6 = {
         ]
     },
 
+    '6_27': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
+
    
 
   
