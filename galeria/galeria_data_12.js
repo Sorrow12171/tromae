3955dea5695206ed92d4c9859b2f3493e976a999
Sorrow12171 <miku12171f@gmail.com>
2026-05-03 13:23:19 -0500

    Crear seccion 12_Putas

--- galeria/galeria_data_12.js
+++ galeria/galeria_data_12.js
@@ -1678,6 +1678,14 @@ const galeriaData_12 = {
             { id: 51, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD29/main/imagenes/img_1775166136224.jpg" }
         ]
     },
+
+    '12_Putas': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
 };
 
 if (typeof galeriaDatabase !== 'undefined') {
