004d7f48956c6f218ef44bae3a41f9f8e37f228a
Sorrow12171 <miku12171f@gmail.com>
2026-05-09 09:09:40 -0500

    Crear galeria_data_36.js

---
+++ galeria/galeria_data_36.js
@@ -0,0 +1,13 @@
+// GALERÍA - DATOS CHICA 36
+const galeriaData_36 = {
+    '36_1': {
+        titulo: "KonoPutas",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD36/main/imagenes/img_1778335761734.jpg",
+        imagenes: []
+    }
+};
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_36);
+}
