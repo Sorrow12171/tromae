bea7d9f1d9ef1129500ad923406116a99445023d
Sorrow12171 <miku12171f@gmail.com>
2026-05-09 09:02:48 -0500

    Crear seccion 34_2

--- galeria/galeria_data_34.js
+++ galeria/galeria_data_34.js
@@ -6,7 +6,15 @@ const galeriaData_34 = {
         categoria: "",
         imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD35/main/imagenes/img_1778335282976.jpg",
         imagenes: []
-    }
+    },
+
+    '34_2': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
 };
 if (typeof galeriaDatabase !== 'undefined') {
     Object.assign(galeriaDatabase, galeriaData_34);
