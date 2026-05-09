f12291f769e15b498fb9b913f28d9bbaf0277a1e
Sorrow12171 <miku12171f@gmail.com>
2026-05-09 10:39:07 -0500

    Crear galeria_data_37.js

---
+++ galeria/galeria_data_37.js
@@ -0,0 +1,13 @@
+// GALERÍA - DATOS CHICA 37
+const galeriaData_37 = {
+    '37_1': {
+        titulo: "GenshinPutas",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD34/main/imagenes/img_1778341130470.jpg",
+        imagenes: []
+    }
+};
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_37);
+}
