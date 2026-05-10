920b21c63cbf1570b6b6d9485788f63b6b98b45b
Sorrow12171 <miku12171f@gmail.com>
2026-05-10 00:16:59 -0500

    Update 1_animes.js

--- 1_animes.js
+++ 1_animes.js
@@ -621,20 +621,7 @@ function existeAnime(contenedor, subcontenedor) {
 }
 
 function obtenerContenedoresAnimesDisponibles() {
-    const contenedores = {
-    // Sub-contenedor 5_7
-    '5_7': {
-        titulo: "Trolas",
-        driveIdEsp: "1G4CdQ9mK-8pp54LgtAZJvk7wgHrrp_em",
-        driveIdJap: "",
-        descripcion: "",
-        timestampsEsp: [],
-        timestampsJap: [],
-        duracion: "0:00",
-        categoria: "",
-        año: 2025
-    },
-};
+    const contenedores = {};
     
     Object.keys(animesDatabase).forEach(key => {
         const [contenedor, subcontenedor] = key.split('_');
