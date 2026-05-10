45094034b2b7ed8d4fbfd9a5258cc5bab28a9fb8
Sorrow12171 <miku12171f@gmail.com>
2026-05-10 09:19:31 -0500

    Update 1_imagenes.js

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -721,13 +721,6 @@ const sistemaDescriptivo = {
                 descripcion: "", 
                 episodios: "" 
             },
-'5_7': { 
-                nombre: 'asd',
-                imagen: 'https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/frame_48900_1778422181311.webp', 
-                descripcion: '', 
-                episodios: '' 
-            }
-
             
             // Contenedor 6
             '6_1': { 
