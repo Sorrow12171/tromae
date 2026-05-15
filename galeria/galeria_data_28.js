694116c71c287657be4a926faab64e528d912bd7
Sorrow12171 <miku12171f@gmail.com>
2026-05-14 20:09:15 -0500

    Crear seccion 28_6

--- galeria/galeria_data_28.js
+++ galeria/galeria_data_28.js
@@ -273,6 +273,14 @@ const galeriaData_28 = {
     { id: 52, url: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD36/main/imagenes/img_1778617870850.jpg" }
 ]
     },
+
+    '28_6': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
 };
 
 if (typeof galeriaDatabase !== 'undefined') {
