2faf66881fc9a806ea3be383befc67d9b6c23f2d
Sorrow12171 <miku12171f@gmail.com>
2026-05-19 17:16:13 -0500

    Crear galeria_data_38.js

---
+++ galeria/galeria_data_38.js
@@ -0,0 +1,13 @@
+// GALERÍA - DATOS CHICA 38
+const galeriaData_38 = {
+    '38_1': {
+        titulo: "BokuNoPutas",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD39/main/imagenes/img_1779228966740.jpg",
+        imagenes: []
+    }
+};
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_38);
+}
