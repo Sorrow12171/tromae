e5cc84b183050c1dbc2d32d489e2a331d32b884f
Sorrow12171 <miku12171f@gmail.com>
2026-05-22 12:48:10 -0500

    Crear seccion 13_28

--- galeria/galeria_data_13.js
+++ galeria/galeria_data_13.js
@@ -1538,6 +1538,14 @@ const galeriaData_13 = {
 ]
     },
 
+    '13_28': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
+
     
 
 
