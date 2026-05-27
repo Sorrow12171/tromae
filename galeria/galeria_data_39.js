98de37cd269e0d4df6998be72ba8c71c402c07bd
Sorrow12171 <miku12171f@gmail.com>
2026-05-27 18:02:40 -0500

    Crear galeria_data_39.js

---
+++ galeria/galeria_data_39.js
@@ -0,0 +1,13 @@
+// GALERÍA - DATOS CHICA 39
+const galeriaData_39 = {
+    '39_1': {
+        titulo: "OnePutas",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD56/main/imagenes/img_1779922951930.jpg",
+        imagenes: []
+    }
+};
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_39);
+}
