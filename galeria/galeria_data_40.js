394d82fd9937f3585308674176cb33b8f96847f1
Sorrow12171 <miku12171f@gmail.com>
2026-05-27 18:29:27 -0500

    Crear galeria_data_40.js

---
+++ galeria/galeria_data_40.js
@@ -0,0 +1,13 @@
+// GALERÍA - DATOS CHICA 40
+const galeriaData_40 = {
+    '40_1': {
+        titulo: "komiputas",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD56/main/imagenes/img_1779924558545.webp",
+        imagenes: []
+    }
+};
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_40);
+}
