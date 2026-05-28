4a0550f45b85c2b932b0ab57f6f7b927c3420f2f
Sorrow12171 <miku12171f@gmail.com>
2026-05-28 14:19:20 -0500

    Agrega imagen 7_36: Culaso de megumin

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -2508,7 +2508,14 @@ const sistemaDescriptivo = {
                 duracion: '' 
             },
             
-                                                                                                                                                                                                                                                                                      '8_1': { 
+            '7_36': { 
+                nombre: 'Culaso de megumin',
+                imagen: 'https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/frame_3000_1779995801712.webp', 
+                descripcion: '', 
+                duracion: '' 
+            },
+            
+                                                                                                                                                                                                                                                                                                  '8_1': { 
                 nombre: 'fabri putardo',
                 imagen: 'https://pbs.twimg.com/media/G_2dDpBX0AAcGz6?format=png&name=small', 
                 descripcion: '', 
