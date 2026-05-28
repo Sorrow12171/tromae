57d7b47feb6131c79eb5d83ee02261abbb4d3170
Sorrow12171 <miku12171f@gmail.com>
2026-05-28 14:19:18 -0500

    Agrega video 7_36: Culaso de megumin con 0 timestamps

--- 1_videos.js
+++ 1_videos.js
@@ -1393,7 +1393,18 @@ const videosDatabase = {
         categoria: "Basico"
     },
 
-                                                                                                     '8_1': {
+
+    // Sub-contenedor 7_36
+    '7_36': {
+        titulo: "Culaso de megumin",
+        driveId: "1ykjKwA_yEidYtH02zyvPIJImjYz_PE5w",
+        descripcion: "",
+        timestamps: [],
+        duracion: "0:00",
+        categoria: "Basico"
+    },
+
+                                                                                                         '8_1': {
         titulo: "Fabri putardo novia yotsuba potona",
         driveId: "1ZOppS-j8KFYmDncgh4A1T9t1Q77tbKS7", // zero two
         descripcion: "Descuide a Yotsuba",
