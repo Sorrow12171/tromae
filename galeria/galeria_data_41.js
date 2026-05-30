fadf2ea999d0e1e131321a0514d9ca6168f2ce36
Sorrow12171 <miku12171f@gmail.com>
2026-05-30 14:02:32 -0500

    Crear galeria_data_41.js

---
+++ galeria/galeria_data_41.js
@@ -0,0 +1,13 @@
+// GALERÍA - DATOS CHICA 41
+const galeriaData_41 = {
+    '41_1': {
+        titulo: "NaruPutas",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD57/main/imagenes/img_1780167748578.jpg",
+        imagenes: []
+    }
+};
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_41);
+}
