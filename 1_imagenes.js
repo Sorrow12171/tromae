96388400298b1da3c4a800d17c387bc3f1c60d6e
Sorrow12171 <miku12171f@gmail.com>
2026-05-31 09:32:50 -0500

    Agrega imagen 7_37: Emilia cachonda

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -2522,7 +2522,14 @@ const sistemaDescriptivo = {
                 duracion: '' 
             },
             
-                                                                                                                                                                                                                                                                                                  '8_1': { 
+            '7_37': { 
+                nombre: 'Emilia cachonda',
+                imagen: 'https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/frame_1710_1780237452112.webp', 
+                descripcion: '', 
+                duracion: '' 
+            },
+            
+                                                                                                                                                                                                                                                                                                              '8_1': { 
                 nombre: 'fabri putardo',
                 imagen: 'https://pbs.twimg.com/media/G_2dDpBX0AAcGz6?format=png&name=small', 
                 descripcion: '', 
