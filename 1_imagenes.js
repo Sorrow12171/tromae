83bbb8cb24664de00d3fadff1db6c9b1e676c73b
Sorrow12171 <miku12171f@gmail.com>
2026-05-31 12:23:05 -0500

    Agrega imagen 7_40: Putona

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -2543,7 +2543,14 @@ const sistemaDescriptivo = {
                 duracion: '' 
             },
             
-                                                                                                                                                                                                                                                                                                                                      '8_1': { 
+            '7_40': { 
+                nombre: 'Putona',
+                imagen: 'https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/frame_4900_1780247980643.webp', 
+                descripcion: '', 
+                duracion: '' 
+            },
+            
+                                                                                                                                                                                                                                                                                                                                                  '8_1': { 
                 nombre: 'fabri putardo',
                 imagen: 'https://pbs.twimg.com/media/G_2dDpBX0AAcGz6?format=png&name=small', 
                 descripcion: '', 
