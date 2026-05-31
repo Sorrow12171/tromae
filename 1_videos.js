d46e5c14411dadab12a5cd0b6a6f97f64ceb9e04
Sorrow12171 <miku12171f@gmail.com>
2026-05-31 09:46:16 -0500

    Agrega video 7_38: Putona con 0 timestamps

--- 1_videos.js
+++ 1_videos.js
@@ -1415,7 +1415,18 @@ const videosDatabase = {
         categoria: "Basico"
     },
 
-                                                                                                             '8_1': {
+
+    // Sub-contenedor 7_38
+    '7_38': {
+        titulo: "Putona",
+        driveId: "1f8hMbyv-vf0VvhCHDuEv-Kb-svwCamn-",
+        descripcion: "",
+        timestamps: [],
+        duracion: "0:00",
+        categoria: "Basico"
+    },
+
+                                                                                                                 '8_1': {
         titulo: "Fabri putardo novia yotsuba potona",
         driveId: "1ZOppS-j8KFYmDncgh4A1T9t1Q77tbKS7", // zero two
         descripcion: "Descuide a Yotsuba",
