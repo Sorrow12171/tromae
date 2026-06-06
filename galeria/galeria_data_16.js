76b7bd688c88e4cc17d45eab6ae076bfaea7238b
Sorrow12171 <miku12171f@gmail.com>
2026-06-06 08:13:21 -0500

    Agregar 1 imagen a 16_1

--- galeria/galeria_data_16.js
+++ galeria/galeria_data_16.js
@@ -12,7 +12,9 @@ const galeriaData_16 = {
         imagenes: [
             { id: 1, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772995724249.jpg" },
             { id: 2, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1773026456160.jpg" }
-        ]
+        
+    { id: 3, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1780751597411_1.png" }
+]
     },
     '16_2': {
         titulo: "Uzaki kachera",
