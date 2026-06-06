acdadb0b65244ef72ea121e4c4aa60056c76d080
Sorrow12171 <miku12171f@gmail.com>
2026-06-06 08:53:12 -0500

    Agregar 3 imgs a 42_1

--- galeria/galeria_data_42.js
+++ galeria/galeria_data_42.js
@@ -5,7 +5,11 @@ const galeriaData_42 = {
         descripcion: "",
         categoria: "",
         imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD60/main/imagenes/img_1780749493554.png",
-        imagenes: []
+        imagenes: [
+    { id: 1, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1780753984684.png" },
+    { id: 2, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1780753986797.png" },
+    { id: 3, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1780753988876.png" }
+]
     }
 };
 if (typeof galeriaDatabase !== 'undefined') {
