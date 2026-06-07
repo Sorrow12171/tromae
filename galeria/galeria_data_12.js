437dad1d327d7ccbbecf5696011b4863b4c34a06
Sorrow12171 <miku12171f@gmail.com>
2026-06-06 20:13:19 -0500

    Update galeria_data_12.js

--- galeria/galeria_data_12.js
+++ galeria/galeria_data_12.js
@@ -32,7 +32,7 @@ const galeriaData_12 = {
             { id: 20, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772902278508.jpg" },
             { id: 21, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772902287515.jpg" },
             { id: 22, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772902312058.jpg" },
-            { id: 23, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772977925237.jpg" }
+            { id: 23, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772977925237.jpg" },
         
             { id: 24, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1780794417844.png" }
 ]
