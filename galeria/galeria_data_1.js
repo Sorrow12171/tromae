45f8e3813419060af7e1dd1ea0fbb95966da2a7f
Sorrow12171 <miku12171f@gmail.com>
2026-06-07 13:27:03 -0500

    Agregar 1 imgs a 1_1

--- galeria/galeria_data_1.js
+++ galeria/galeria_data_1.js
@@ -266,7 +266,9 @@ const galeriaData_1 = {
             { id: 154, url: "https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/img_1774920436849_1.png" },
             { id: 155, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1776630311500_1.png" },
             { id: 156, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1780341303612_1.png" }
-        ]
+        
+            { id: 157, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1780856816602.png" }
+]
     },
     '1_2': {
         titulo: "Nino Parque de las putas",
