034bd26545b4d549293336e5a4144d2cd84512d5
Sorrow12171 <miku12171f@gmail.com>
2026-06-09 17:31:11 -0500

    Crear galeria_data_43.js

---
+++ galeria/galeria_data_43.js
@@ -0,0 +1,13 @@
+// GALERÍA - DATOS CHICA 43
+const galeriaData_43 = {
+    '43_1': {
+        titulo: "OriPuta",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD65/main/imagenes/img_1781044263847.jpg",
+        imagenes: []
+    }
+};
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_43);
+}
