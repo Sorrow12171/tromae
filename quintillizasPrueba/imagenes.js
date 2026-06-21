e8b9cb3a79b3eb4a66916c26d8ea42664758de65
Sorrow12171 <miku12171f@gmail.com>
2026-06-20 23:19:56 -0500

    Update image URLs and audio links for anal categories

--- quintillizasPrueba/imagenes.js
+++ quintillizasPrueba/imagenes.js
@@ -33,9 +33,9 @@ reverse_cowgirl: { url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDD
     reverse_cowgirl_le_estiro_el_ano: { url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD21/main/imagenes/img_1773692629321.jpg", audio: "https://files.catbox.moe/t4r8hw.mp3" },        
     sidefuck: { url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD21/main/imagenes/img_1773692689236.jpg", audio: "" },
               sidefuck_de_pie: { url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1782008568472.png", audio: "" },
-    follando_anal: { url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD21/main/imagenes/img_1773757229891.jpg", audio: "https://files.catbox.moe/rvwtyd.mp3" },  
-    follando_anal_rapido: { url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD21/main/imagenes/img_1773757229891.jpg", audio: "https://files.catbox.moe/hhewjq.mp3" },  
-    anal_cumming: { url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD21/main/imagenes/img_1773757227550.jpg", audio: "" },
+    follando_anal: { url: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD59/main/imagenes/img_1780230442588.jpg", audio: "https://files.catbox.moe/rvwtyd.mp3" },  
+    follando_anal_rapido: { url: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD59/main/imagenes/img_1780230442588.jpg", audio: "https://files.catbox.moe/hhewjq.mp3" },  
+    anal_cumming: { url: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD59/main/imagenes/img_1780230440717.jpg", audio: "" },
     ichika_licking_anus: { url: "https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/img_1774103171064.png", audio: "" },
   
             follando_en_el_aire: { url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD23/main/imagenes/img_1774198366172.jpg", audio: "" }, 
