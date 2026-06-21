4852e44d14ddeacba49c273df3d496579056b0ac
Sorrow12171 <miku12171f@gmail.com>
2026-06-21 09:57:41 -0500

    Update logica.js

--- quintillizasPrueba/logica.js
+++ quintillizasPrueba/logica.js
@@ -46,7 +46,7 @@ const _K = [
 ];
 
 const GROQ_KEYS = _K.map(partes => partes.join(""));
-const MODELO_PRINCIPAL = "qwen/qwen3.6-27b";
+const MODELO_PRINCIPAL = "qwen/qwen3-32b";
 const MODELO_VISION = ""; // Modelo de Groq con soporte de visión
 
 let indiceKeyActual = 0;
