0f660065749aa0fcca2405d2ab4b3c560943315a
Sorrow12171 <miku12171f@gmail.com>
2026-07-04 22:39:52 -0500

    Agregar 6 imgs a 44_1

--- galeria/galeria_data_44.js
+++ galeria/galeria_data_44.js
@@ -5,7 +5,14 @@ const galeriaData_44 = {
         descripcion: "",
         categoria: "",
         imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD69/main/imagenes/img_1783220485958.png",
-        imagenes: []
+        imagenes: [
+    { id: 1, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1783222775829.png" },
+    { id: 2, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1783222778444.png" },
+    { id: 3, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1783222780922.png" },
+    { id: 4, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1783222783313.png" },
+    { id: 5, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1783222786265.png" },
+    { id: 6, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1783222788174.png" }
+]
     }
 };
 if (typeof galeriaDatabase !== 'undefined') {
