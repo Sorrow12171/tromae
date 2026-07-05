c4347db7a51b96234fbd16cd76ba333ebb0163d5
Sorrow12171 <miku12171f@gmail.com>
2026-07-04 22:01:30 -0500

    Crear galeria_data_44.js

---
+++ galeria/galeria_data_44.js
@@ -0,0 +1,13 @@
+// GALERÍA - DATOS CHICA 44
+const galeriaData_44 = {
+    '44_1': {
+        titulo: "poketrolas",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD69/main/imagenes/img_1783220485958.png",
+        imagenes: []
+    }
+};
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_44);
+}
