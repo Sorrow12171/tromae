02b8723c80527bd75f4c846868a83d73d49a8251
Sorrow12171 <miku12171f@gmail.com>
2026-07-22 09:50:01 -0500

    Crear seccion 38_14

--- galeria/galeria_data_38.js
+++ galeria/galeria_data_38.js
@@ -591,6 +591,15 @@ const galeriaData_38 = {
     { id: 113, url: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD70/main/imagenes/img_1784727347103.jpg" }
 ]
     },
+
+    '38_14': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes_rotacion: [],
+        imagenes: []
+    },
 };
 if (typeof galeriaDatabase !== 'undefined') {
     Object.assign(galeriaDatabase, galeriaData_38);
