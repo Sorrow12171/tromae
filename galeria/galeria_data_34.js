abfbaeaa18adba1bd58cd1f05d3a7c6230ca3569
Sorrow12171 <miku12171f@gmail.com>
2026-08-05 13:45:01 -0500

    Agregar 2 imgs a 34_1

--- galeria/galeria_data_34.js
+++ galeria/galeria_data_34.js
@@ -62,7 +62,9 @@ const galeriaData_34 = {
     { id: 53, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1784934519222.png" },
     { id: 54, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1784934521610.png" },
     { id: 55, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1784934523604.png" },
-    { id: 56, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1785637108527.png" }
+    { id: 56, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1785637108527.png" },
+    { id: 57, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1785955495141.png" },
+    { id: 58, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1785955497811.png" }
 ]
     },
 
