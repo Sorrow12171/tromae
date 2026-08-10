d405092d1fd93bdf13036ee9611ff4331e454854
Sorrow12171 <miku12171f@gmail.com>
2026-08-10 14:42:00 -0500

    Crear galeria_data_46.js

---
+++ galeria/galeria_data_46.js
@@ -0,0 +1,13 @@
+// GALERÍA - DATOS CHICA 46
+const galeriaData_46 = {
+    '46_1': {
+        titulo: "Mushoku trolas",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD70/main/imagenes/img_1786390917747.jpg",
+        imagenes: []
+    }
+};
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_46);
+}
