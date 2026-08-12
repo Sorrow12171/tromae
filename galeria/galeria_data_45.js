35edcc6a162fce223da0261465dfeaf3f5826a57
Sorrow12171 <miku12171f@gmail.com>
2026-08-11 21:32:05 -0500

    Crear seccion 45_2

--- galeria/galeria_data_45.js
+++ galeria/galeria_data_45.js
@@ -38,7 +38,16 @@ const galeriaData_45 = {
     { id: 30, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1785955568343.png" },
     { id: 31, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1785955570974.png" }
 ]
-    }
+    },
+
+    '45_2': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes_rotacion: [],
+        imagenes: []
+    },
 };
 if (typeof galeriaDatabase !== 'undefined') {
     Object.assign(galeriaDatabase, galeriaData_45);
