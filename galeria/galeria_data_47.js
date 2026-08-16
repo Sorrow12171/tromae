8175e9067227f282ab69f93b99e4e16ff07c95cd
Sorrow12171 <miku12171f@gmail.com>
2026-08-16 14:41:34 -0500

    Crear galeria_data_47.js

---
+++ galeria/galeria_data_47.js
@@ -0,0 +1,13 @@
+// GALERÍA - DATOS CHICA 47
+const galeriaData_47 = {
+    '47_1': {
+        titulo: "Noelle putasilva",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD72/main/imagenes/img_1786909291728.png",
+        imagenes: []
+    }
+};
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_47);
+}
