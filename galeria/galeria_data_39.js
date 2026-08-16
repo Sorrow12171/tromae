e4cada3d16542a60992c80b8b12022d951e2eac8
Sorrow12171 <miku12171f@gmail.com>
2026-08-16 15:48:32 -0500

    Agregar 1 imgs a 39_1

--- galeria/galeria_data_39.js
+++ galeria/galeria_data_39.js
@@ -10,7 +10,8 @@ const galeriaData_39 = {
     { id: 2, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1782521082343.png" },
     { id: 3, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1784504503887.png" },
     { id: 4, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1784504507208.png" },
-    { id: 5, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1785955509069.png" }
+    { id: 5, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1785955509069.png" },
+    { id: 6, url: "https://raw.githubusercontent.com/Sorrow12171/imaginardas/main/imagenes/img_1786913309011.png" }
 ]
     },
 
