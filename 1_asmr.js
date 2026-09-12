7e0d0f790a48f2fa405940a8894fa7ed25925205
Sorrow12171 <miku12171f@gmail.com>
2026-09-12 16:29:51 -0500

    Agrega ASMR 1_9: grabando porno 2 con 0 timestamps

--- 1_asmr.js
+++ 1_asmr.js
@@ -140,7 +140,21 @@ const asmrDatabase = {
         tags: []
     },
 
-                    '2_1': {
+
+    // Sub-contenedor 1_9
+    '1_9': {
+        titulo: "grabando porno 2",
+        driveId: "1AxIsYkuHFkZfRgpYDURK14wemVacNN4C",
+        descripcion: "",
+        timestamps: [],
+        duracion: "0:00",
+        categoria: "",
+        tipoVoz: "",
+        tipo: "audio",
+        tags: []
+    },
+
+                        '2_1': {
         titulo: "🎌 Repetición de Vocabulario N5 (ASMR)",
         driveId: "1tS-gKr6bf4MY5Yrw7zRvP2uP_zq9rsLl",
         descripcion: "Repetición suave de palabras esenciales del nivel N5 con pausas para practicar.",
