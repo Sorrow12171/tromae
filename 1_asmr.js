c1cf91c260fd796417a30d61151106f233be982f
Sorrow12171 <miku12171f@gmail.com>
2026-09-12 17:17:05 -0500

    Agrega ASMR 3_11: Peteada epica con 0 timestamps

--- 1_asmr.js
+++ 1_asmr.js
@@ -437,7 +437,21 @@ const asmrDatabase = {
         tags: []
     },
 
-                    '4_1': {
+
+    // Sub-contenedor 3_11
+    '3_11': {
+        titulo: "Peteada epica",
+        driveId: "15vDK1RCHuk69oL5emhnLwxsHlOasoVfQ",
+        descripcion: "",
+        timestamps: [],
+        duracion: "0:00",
+        categoria: "",
+        tipoVoz: "",
+        tipo: "audio",
+        tags: []
+    },
+
+                        '4_1': {
         titulo: "Alya :D",
         driveId: "1RH9vN9auoN1GR7svQ78wEkMqT2oMWHwW",
         descripcion: "Historia suave con ritmo lento diseñada para ayudar a conciliar el sueño.",
