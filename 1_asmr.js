f7000c05d80db120289cce1670de5c126e60a11e
Sorrow12171 <miku12171f@gmail.com>
2026-09-12 17:25:24 -0500

    Agrega ASMR 1_14: Vecina petera con 0 timestamps

--- 1_asmr.js
+++ 1_asmr.js
@@ -210,7 +210,21 @@ const asmrDatabase = {
         tags: []
     },
 
-                                        '2_1': {
+
+    // Sub-contenedor 1_14
+    '1_14': {
+        titulo: "Vecina petera",
+        driveId: "1tP9pwOa0LugrRaUYiE4AySxVWNUUASCQ",
+        descripcion: "",
+        timestamps: [],
+        duracion: "0:00",
+        categoria: "",
+        tipoVoz: "",
+        tipo: "audio",
+        tags: []
+    },
+
+                                            '2_1': {
         titulo: "🎌 Repetición de Vocabulario N5 (ASMR)",
         driveId: "1tS-gKr6bf4MY5Yrw7zRvP2uP_zq9rsLl",
         descripcion: "Repetición suave de palabras esenciales del nivel N5 con pausas para practicar.",
