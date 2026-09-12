d72f0fdb8214b75cd253915257ae30ea5d6158e3
Sorrow12171 <miku12171f@gmail.com>
2026-09-12 18:07:25 -0500

    Agrega ASMR 1_15: Devuelve favor peteando con 0 timestamps

--- 1_asmr.js
+++ 1_asmr.js
@@ -224,7 +224,21 @@ const asmrDatabase = {
         tags: []
     },
 
-                                            '2_1': {
+
+    // Sub-contenedor 1_15
+    '1_15': {
+        titulo: "Devuelve favor peteando",
+        driveId: "1HziL9ksOfgnG1I_TOdtXyx5HlgiH9xtP",
+        descripcion: "",
+        timestamps: [],
+        duracion: "0:00",
+        categoria: "",
+        tipoVoz: "",
+        tipo: "audio",
+        tags: []
+    },
+
+                                                '2_1': {
         titulo: "🎌 Repetición de Vocabulario N5 (ASMR)",
         driveId: "1tS-gKr6bf4MY5Yrw7zRvP2uP_zq9rsLl",
         descripcion: "Repetición suave de palabras esenciales del nivel N5 con pausas para practicar.",
