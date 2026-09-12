71fb6cf6acb5f14bb60e8d0c59c295bad35815e8
Sorrow12171 <miku12171f@gmail.com>
2026-09-12 16:11:39 -0500

    Agrega ASMR 1_6: Alumna pajeadora con 0 timestamps

--- 1_asmr.js
+++ 1_asmr.js
@@ -98,7 +98,21 @@ const asmrDatabase = {
         tags: []
     },
 
-        '2_1': {
+
+    // Sub-contenedor 1_6
+    '1_6': {
+        titulo: "Alumna pajeadora",
+        driveId: "1R2bLm2uw_M4EyCXbZN4QbrjdxtdacSQG",
+        descripcion: "",
+        timestamps: [],
+        duracion: "0:00",
+        categoria: "",
+        tipoVoz: "",
+        tipo: "audio",
+        tags: []
+    },
+
+            '2_1': {
         titulo: "🎌 Repetición de Vocabulario N5 (ASMR)",
         driveId: "1tS-gKr6bf4MY5Yrw7zRvP2uP_zq9rsLl",
         descripcion: "Repetición suave de palabras esenciales del nivel N5 con pausas para practicar.",
