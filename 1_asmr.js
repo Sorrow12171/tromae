61fb03d1a0d00d7af464913e4f47a342bcf86f64
Sorrow12171 <miku12171f@gmail.com>
2026-09-12 17:10:50 -0500

    Agrega ASMR 3_10: mama se coge a mi capitan de futbol con 0 timestamps

--- 1_asmr.js
+++ 1_asmr.js
@@ -423,7 +423,21 @@ const asmrDatabase = {
         tags: []
     },
 
-                '4_1': {
+
+    // Sub-contenedor 3_10
+    '3_10': {
+        titulo: "mama se coge a mi capitan de futbol",
+        driveId: "1K4SrCyVFGw8h8qMeGmbNjqHz0R1EBh6j",
+        descripcion: "",
+        timestamps: [],
+        duracion: "0:00",
+        categoria: "",
+        tipoVoz: "",
+        tipo: "audio",
+        tags: []
+    },
+
+                    '4_1': {
         titulo: "Alya :D",
         driveId: "1RH9vN9auoN1GR7svQ78wEkMqT2oMWHwW",
         descripcion: "Historia suave con ritmo lento diseñada para ayudar a conciliar el sueño.",
