2cb8b550af201f0b3b8cc9823c0daa72f036f778
Sorrow12171 <miku12171f@gmail.com>
2026-09-12 16:50:35 -0500

    Agrega portada ASMR 1_12: milf petera

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -1453,7 +1453,14 @@ const sistemaDescriptivo = {
                 tipoVoz: '' 
             },
             
-                                                                                                '2_1': { 
+            '1_12': { 
+                nombre: 'milf petera',
+                imagen: '', 
+                descripcion: '', 
+                tipoVoz: '' 
+            },
+            
+                                                                                                            '2_1': { 
                 nombre: 'ASMR 1',
                 imagen: '', 
                 descripcion: '', 
