a4f8b7632ca57792802c6e5e7d6f14676ee311cb
Sorrow12171 <miku12171f@gmail.com>
2026-09-12 17:07:18 -0500

    Agrega ASMR 1_13: Compañera me hace assjob con 0 timestamps

--- 1_asmr.js
+++ 1_asmr.js
@@ -196,7 +196,21 @@ const asmrDatabase = {
         tags: []
     },
 
-                                    '2_1': {
+
+    // Sub-contenedor 1_13
+    '1_13': {
+        titulo: "Compañera me hace assjob",
+        driveId: "1_09SK0o0FqriWESPxMyhMEmOd0HXLwOC",
+        descripcion: "",
+        timestamps: [],
+        duracion: "0:00",
+        categoria: "",
+        tipoVoz: "",
+        tipo: "audio",
+        tags: []
+    },
+
+                                        '2_1': {
         titulo: "🎌 Repetición de Vocabulario N5 (ASMR)",
         driveId: "1tS-gKr6bf4MY5Yrw7zRvP2uP_zq9rsLl",
         descripcion: "Repetición suave de palabras esenciales del nivel N5 con pausas para practicar.",
