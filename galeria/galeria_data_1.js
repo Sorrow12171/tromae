4ca8b6b36419bfe195f718a8595265d5c9aafff6
Sorrow12171 <miku12171f@gmail.com>
2026-09-26 08:29:14 -0500

    Crear seccion 1_79

--- galeria/galeria_data_1.js
+++ galeria/galeria_data_1.js
@@ -7901,6 +7901,15 @@ const galeriaData_1 = {
     { id: 104, url: "https://raw.githubusercontent.com/Sorrow12171/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD83/main/imagenes/img_1790427764855.webp" }
 ]
     },
+
+    '1_79': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes_rotacion: [],
+        imagenes: []
+    },
    
     
     // CONTENEDOR 2
